5d081f07e04ebf05b7b3370bf83e79f2088082a4
Ben Williams <kleheni@gmail.com>
2020-10-26 15:52:52 -0800

    functionals!

--- R/proj_data.R
+++ R/proj_data.R
@@ -10,21 +10,28 @@
 #' @examples
 proj_data <- function(year, model, model_name){
 
+  # data files
   REP = readLines(here::here(year, "base", paste0(model_name, ".rep")))
-
   proj = readLines(here::here(year, model, "proj.dat"), warn=FALSE)
 
-  age = strsplit(proj[grep("#_Natural_Mortality", proj)], " ")[[1]]
-  age = as.numeric(age[2:length(age)])
+  # helper function
+  split_item <- function(name, one = NULL){
 
-  naa = strsplit(proj[grep("#_Numbers_at_age_end_year", proj) + 1], " ")[[1]]
-  naa = as.numeric(naa[2:length(naa)])
+    if(is.null(one)){
+    x = strsplit(proj[grep(name, proj)], " ")[[1]]
+    as.numeric(x[2:length(x)])
+    } else {
+      x = strsplit(proj[grep(name, proj) + 1], " ")[[1]]
+      as.numeric(x[2:length(x)])
+    }
+  }
 
-  waa = strsplit(proj[grep("#_Wt_at_age_spawners", proj) + 1], " ")[[1]]
-  waa = as.numeric(waa[2:length(waa)])
+  # pull data
+  age = split_item("#_Natural_Mortality")
+  naa = split_item("#_Numbers_at_age_end_year", 1)
+  waa = split_item("#_Wt_at_age_spawners", 1)
+  saa = split_item("#_Selectivity_fishery_scaled_to_max_at_one", 1)
 
-  saa = strsplit(proj[grep("#_Selectivity_fishery_scaled_to_max_at_one", proj) + 1], " ")[[1]]
-  saa = as.numeric(saa[2:length(saa)])
   m = as.numeric(strsplit(proj[grep("#_Natural_Mortality", proj) + 1], " ")[[1]][[2]])
   ofl = as.numeric(strsplit(REP[grep(paste0("OFL for ", year-1), REP) + 1] [[2]]," "))
 
